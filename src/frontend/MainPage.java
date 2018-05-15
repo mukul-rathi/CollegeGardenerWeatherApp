b5b11613c667e09fda12cba09ffb3381ae9b60f2
drsphelps <drsphelps@gmail.com>
2018-05-15 14:42:12 +0100

    Added Initial Front End
    Main file to launch app.
    MainPage fxml and controller to describe the initial page the user will see.

---
+++ src/frontend/MainPage.java
@@ -0,0 +1,13 @@
+package practice;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainPage implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
