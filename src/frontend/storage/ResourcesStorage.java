c38d9a441e251c802ffc52e6623722e2c9a0442b
ben <bp413@cam.ac.uk>
2018-05-20 21:03:43 +0100

    Storing and Styling Alerts
    (IMPORTANT NOTE AT BOTTOM)
    The Alerts Pane has been styled in line with the long view styling and the main page
    
    Now the Storage Classes store the alerts data as well, and the users prefernce is loaded when you go to the alerts tab.
    
    This data can be fetched at any point in the application, for whoever implements the alerts.
    
    The alerts information is now returned as two arrays of booleans, for the five weather types in the alerts pane. True means the radio button is selected, False means it is not.
    
    IMPORTANT NOTE:
    When testing you NEED to delete the file storage.xml in your local directory (unless you copy the one I have pushed)
    Then when you run the program, the code will detect that it is missing and build a new one with a default setting for the alerts data in it
    Test that you can edit the alerts radio buttons, press back, quit the program, re-load and see that the selection you made is retained.

--- src/frontend/storage/ResourcesStorage.java
+++ src/frontend/storage/ResourcesStorage.java
@@ -1,9 +1,13 @@
 package frontend.storage;
 
 
+import backend.WeatherType;
+import frontend.AlertsContext;
+
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.HashMap;
 
 @XmlRootElement
 public class ResourcesStorage {
@@ -14,6 +18,12 @@ public class ResourcesStorage {
     private boolean tempScale; // true is centigrade, false is fahrenheit
     private boolean speedScale; // true is mph, false is kph
     private String language; // the language the user wants
+    private boolean[] alertable;
+    private boolean[] priority;
+
+    // Weather type used for alerts
+    final WeatherType[] alertTypes = new WeatherType[]{WeatherType.RAIN, WeatherType.SLEET, WeatherType.SNOW,
+            WeatherType.WIND, WeatherType.FOG};
 
 
     public int getId() {
@@ -60,5 +70,56 @@ public class ResourcesStorage {
     @XmlElement
     public void setLanguage(String language) {
         this.language = language;
+    }
+
+    /*
+    public AlertsContext getAlerts() {
+
+        HashMap<WeatherType, Boolean> altertable = new HashMap<>();
+        HashMap<WeatherType, Boolean> priority = new HashMap<>();
+
+        for(int i = 0; i < getPriority().length; i++){
+            altertable.put(alertTypes[i], getAlertable()[i]);
+            priority.put(alertTypes[i], getPriority()[i]);
+        }
+
+        AlertsContext alerts = new AlertsContext(altertable,priority);
+
+        return alerts;
+    }
+    */
+
+
+    public void setAlerts(AlertsContext alerts) {
+        HashMap<WeatherType, Boolean> userAlertable = alerts.getAltertable();
+        HashMap<WeatherType, Boolean> userPriority = alerts.getPriority();
+        //Setup arrays with the correct values
+        for(int i = 0; i < alertTypes.length; i++){
+            WeatherType w = alertTypes[i];
+            alertable[i] = userAlertable.get(w);
+            priority[i] = userPriority.get(w);
+        }
+
+    }
+
+
+    public boolean[] getAlertable() {
+
+        return alertable;
+    }
+
+    @XmlElement
+    public void setAlertable(boolean[] alertable) {
+        this.alertable = alertable;
+    }
+
+
+    public boolean[] getPriority() {
+        return priority;
+    }
+
+    @XmlElement
+    public void setPriority(boolean[] priority) {
+        this.priority = priority;
     }
 }
